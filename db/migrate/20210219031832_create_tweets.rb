ced35ca83336b182cc2a669261f4a95f85ed1236
sasaki <hakanaidomu@gmail.com>
2021-02-19 13:35:55 +0900

    postテーブルの作成、バリデーションの記述

--- db/migrate/20210219031832_create_tweets.rb
+++
@@ -1,8 +0,0 @@
-class CreatePosts < ActiveRecord::Migration[6.0]
-  def change
-    create_table :tweets do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_084532) do
+ActiveRecord::Schema.define(version: 2021_02_19_031832) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "price"
+    t.integer "calorie"
+    t.integer "likes_cound"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -26,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_02_17_084532) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
