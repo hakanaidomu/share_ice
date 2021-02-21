17638b683c8a6320301f5d8851c50d3fc04367d0
sasaki <hakanaidomu@gmail.com>
2021-02-21 12:42:20 +0900

    テーブルの追加 #17

--- db/migrate/20210221033439_create_profiles.rb
+++ db/migrate/20210221033439_create_profiles.rb
@@ -1,7 +1,9 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-
+      t.text       :discription
+      t.integer    :total_price
+      t.integer    :total_calorie
       t.timestamps
     end
   end
