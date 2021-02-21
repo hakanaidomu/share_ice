7259b1fe75b723f4e01688683fd5a6ac141279e8
sasaki <hakanaidomu@gmail.com>
2021-02-21 12:46:10 +0900

    アソシエーションの記述 #17

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-
+  has_many :posts
+  has_one  :profile
   validates :nickname, presence: true
   validates :email, length: { maximum: 50 }, presence: true, uniqueness: true
 end
