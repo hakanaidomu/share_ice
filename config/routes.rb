6d701f4aba706948869656c6780f3102cd0ecb83
sasaki <hakanaidomu@gmail.com>
2021-02-22 22:47:37 +0900

    非同期によるコメント機能を追加 #22

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   resources :posts do
-    resources :comments
+    resources :comments, only: [:create, :destroy]
   end
   get '/users/:id', to: 'users#show', as: 'user'
 end
