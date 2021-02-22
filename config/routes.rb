e694fd860e12c745840ea6d32e7f658d01fb6672
sasaki <hakanaidomu@gmail.com>
2021-02-22 18:43:34 +0900

    Revert "Merge pull request #20 from hakanaidomu/コメント機能の実装-#19"
    This reverts commit bf447e99bb40ab58adc396fff5786764a43cac0c, reversing
    changes made to 616593199dd81b989b4df51f5898989bff3fe9a0.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
-  resources :posts do
-    resources :comments, only: [:create, :destroy]
-  end
+  resources :posts
   get '/users/:id', to: 'users#show', as: 'user'
 end
