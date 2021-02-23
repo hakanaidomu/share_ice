67161d790dc372ea487b8a0b1109f8eb7ed43d80
sasaki <hakanaidomu@gmail.com>
2021-02-23 14:04:15 +0900

    :tag_listを記述 #27

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,7 +43,7 @@ class PostsController < ApplicationController
 
   private 
   def post_params
-    params.require(:post).permit(:image, :content, :calorie, :price).merge(user_id: current_user.id)
+    params.require(:post).permit(:image, :content, :calorie, :price, :tag_list).merge(user_id: current_user.id)
   end
 
   def set_post
