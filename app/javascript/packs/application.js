549c50e65f063800f0cf6595ed103206adc893c3
sasaki <hakanaidomu@gmail.com>
2021-02-24 17:18:37 +0900

    Revert "テスト"
    This reverts commit 4f0cb9840dbfd9370f469799d27caa9daedc0b57.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,6 @@ require("@rails/activestorage").start()
 require("channels")
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
-//= require tag-it
-//= require_tree .
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
