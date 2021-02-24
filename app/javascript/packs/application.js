bff3d277b7886aef502525b624a6a594eb724ddd
sasaki <hakanaidomu@gmail.com>
2021-02-24 18:10:48 +0900

    TagsInputの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("@rails/activestorage").start()
 require("channels")
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-tagsinput.min
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
