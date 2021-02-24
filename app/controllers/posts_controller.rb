ec39f745aa9f74901a723e4357a00a678e8a585d
sasaki <hakanaidomu@gmail.com>
2021-02-24 14:04:14 +0900

    トップページに上位のタグを表示するように記述 #29

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   before_action :redirect, only: [:edit, :update, :destroy]
   
   def index
+    @tags = Post.tag_counts_on(:tags).most_used(20)
     if params[:tag]
       @posts = Post.tagged_with(params[:tag])
     else
