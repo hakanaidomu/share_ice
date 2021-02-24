f64461d67754cbe6c4774a6cc790e39bab925c91
sasaki <hakanaidomu@gmail.com>
2021-02-24 10:47:10 +0900

    Revert "テスト"
    This reverts commit 0550882034aece3926b05423d29313acff69ce0a.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,6 @@ require("channels")
 //= require jquery_ujs
 
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
