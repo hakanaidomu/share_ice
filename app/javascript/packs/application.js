1f40371ac958f94adb74713ad10f5b2563052d98
sasaki <hakanaidomu@gmail.com>
2021-02-24 15:42:24 +0900

    Revert "ミス"
    This reverts commit 14f42b2942a5dc3f50e8028ad2785da7bc44e1cd.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
 
