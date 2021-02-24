87347c524682f04978d54b2b466111b679d38f6d
sasaki <hakanaidomu@gmail.com>
2021-02-24 10:46:49 +0900

    Revert "if文を用いた微修正"
    This reverts commit 9721661d00391b923b4d31d81eebbf6e103fd44e.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,5 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liking_users, through: :likes, source: :user
   has_one_attached :image
-  validates :content, presence: true
   acts_as_taggable
 end
