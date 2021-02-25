de4a42bab6d8e9de432e69c6daece94408de9e5f
sasaki <hakanaidomu@gmail.com>
2021-02-25 14:27:07 +0900

    _headerで呼び出せるように修正 #33

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,10 +10,12 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-tagsinput.min
-//= require bootstrap-tagsinput
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
