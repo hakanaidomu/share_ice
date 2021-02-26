715b81474365ac192b04ff59ff0dcbdf39de78f6
sasaki <hakanaidomu@gmail.com>
2021-02-27 01:56:37 +0900

    taginputの導入 #33

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,19 @@
 import 'bootstrap';
 import '../stylesheets/application';
 import '@fortawesome/fontawesome-free/js/all';
-
-
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require("bootstrap-tagsinput")
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
-//= require rails-ujs
 //= require turbolinks
 //= require_tree .
 
