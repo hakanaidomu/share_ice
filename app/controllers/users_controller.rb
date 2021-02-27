b7b33c11c6bb357758d856515286143089031cbe
sasaki <hakanaidomu@gmail.com>
2021-02-27 20:38:00 +0900

    ページネーションのデザイン変更 #37

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.page(params[:page])
+    @posts = @user.posts.page(params[:page]).per(10)
     @total_price = @posts.all.sum(:price)
     @total_calorie = @posts.all.sum(:calorie)
   end
