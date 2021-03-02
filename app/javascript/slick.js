8a13bf23ce4205c78388814c398fa6e6b8f18e94
sasaki <hakanaidomu@gmail.com>
2021-03-02 14:14:19 +0900

    centerModeの記述 #41

--- app/javascript/slick.js
+++ app/javascript/slick.js
@@ -1,5 +1,7 @@
 $(function() {
   $('.slider').slick({
+      centerMode: true,
+      centerPadding: '15%',
       dots: true, //スライドの下にドットのナビゲーションを表示
       autoplay: true, //自動再生
       autoplaySpeed: 2000, //再生スピード
