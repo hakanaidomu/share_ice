69472092433ba74423b2b10fe3318b268a845d42
sasaki <hakanaidomu@gmail.com>
2021-03-02 17:05:25 +0900

    スライドショーの修正 #41

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   before_action :redirect, only: [:edit, :update, :destroy]
   
   def index
-      @randams = Post.order("RAND()").limit(5)
+      @randams = Post.order("RAND()").limit(10)
     @tags = Post.tag_counts_on(:tags).most_used(20)
     if params[:tag]
       @posts = Post.tagged_with(params[:tag]).page(params[:page]).per(6)
