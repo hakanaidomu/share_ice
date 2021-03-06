959dba443635099fa629442f0128bffbfd6afe40
sasaki <hakanaidomu@gmail.com>
2021-03-07 03:38:21 +0900

    Update sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,21 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :check_guest, only: [:edit, :update, :destroy]
 
-  def create
-    auth_options = { scope: resource_name, recall: "#{controller_path}#failed" }
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  end
-
-  def failed
-    flash[:alert] = "メールアドレスまたはパスワードが違います。"
-    redirect_to params[:user][:url]
-  end
 
   def new_guest
     user = User.guest
@@ -24,25 +10,11 @@ class Users::SessionsController < Devise::SessionsController
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 
-  def check_guest
-    if resource.email == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーはユーザー情報を編集できません。'
-    end
-  end
+  
 
-  def create
-    auth_options = { scope: resource_name, recall: "#{controller_path}#failed" }
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  end
+  
 
-  def failed
-    flash[:alert] = "メールアドレスまたはパスワードが違います。"
-    redirect_to params[:user][:url]
-  end
+
   # GET /resource/sign_in
   # def new
   #   super
