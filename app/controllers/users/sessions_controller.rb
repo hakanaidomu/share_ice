354b240b2a9abd04ff18b32643361175a72723de
sasaki <hakanaidomu@gmail.com>
2021-03-07 04:04:44 +0900

    詳細画面に行けるように修正 #47

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,19 +24,7 @@ class Users::SessionsController < Devise::SessionsController
   end
 
 
-  def create
-    auth_options = { scope: resource_name, recall: "#{controller_path}#failed" }
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  end
 
-  def failed
-    flash[:alert] = "メールアドレスまたはパスワードが違います。"
-    redirect_to params[:user][:url]
-  end
   # GET /resource/sign_in
   # def new
   #   super
