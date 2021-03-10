9f1e59708ab38a85490d540656e0cbc6e93ef8fa
sasaki <hakanaidomu@gmail.com>
2021-03-10 17:25:14 +0900

    バリデーション修正 #50

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,9 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liking_users, through: :likes, source: :user
   has_one_attached :image
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 150 }
   validates :image, presence: true
+  validates :price, numericality: { less_than_or_equal_to: 9_999 }
+  validates :calorie, numericality: { less_than_or_equal_to: 9_999 }
   acts_as_taggable
 end
