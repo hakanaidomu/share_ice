831eb9b7d0f7e8afc128943b18438201e4a3b88f
sasaki <hakanaidomu@gmail.com>
2021-03-14 12:29:49 +0900

    bundle exec rubocop --auto-correctの実行

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 describe PostsController, type: :request do
-
   before do
     @tweet = FactoryBot.create(:post)
   end
 
   describe 'GET #index' do
-    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
       get root_path
       expect(response.status).to eq(200)
     end
